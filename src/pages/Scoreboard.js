b4d3104a0d298bc859d6f81e9bc90e8bd85d8a0b
Your Name <you@example.com>
2019-04-27 18:04:27 +0900

    페이지 라우팅 설정

---
+++ src/pages/Scoreboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Scoreboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div>
+        Scoreboard works.
+      </div>
+    )
+  }
+
+}
